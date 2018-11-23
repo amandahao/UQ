e62340fb01ba1d6cccba7bd066d4818052f47616
Amanda Hao <amandahao@gmail.com>
2018-11-22 21:29:10 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express')
+	, path = require('path')
+	, bodyParser = require('body-parser')
+	, http = require('http')
+	, async = require('async')
+	, fs = require('fs')
+;
+
+var app = express()
+	, port = parseInt(process.env.PORT || '8080')
+	, server = http.createServer(app)
+;
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+	console.log('home')
+	var filePath = path.join(__dirname, './home.html')
+	res.sendFile(filePath);
+});
+
+app.get('/navbar.css', (req, res, next) => {
+	var filePath = path.join(__dirname, './navbar.css')
+	res.sendFile(filePath);
+})
+
+server.on('listening', () => {
+	var addr = server.address()
+		, bind = typeof addr === 'string'
+			? 'pipe ' + addr
+			: 'port ' + addr.port
+	;
+	console.log('Listening on ' + bind);
+});
+
+server.listen(port);
